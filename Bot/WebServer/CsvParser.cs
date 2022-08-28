a76a652876309be6b6dc5dc97df40f661256ab4c
zibas <milo.brontesi@gmail.com>
2022-08-28 20:58:22 +0200

    Implemented Csv parser

---
+++ Bot/WebServer/CsvParser.cs
@@ -0,0 +1,31 @@
+using System.Globalization;
+using Bot.Database.Entity;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace Bot.WebServer;
+
+public static class CsvParser
+{
+    public static async Task<List<Tutor>> ParseTutors(Stream cvsStream)
+    {
+        TextReader reader = new StreamReader(cvsStream);
+        var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
+        csvReader.Context.RegisterClassMap<CsvTutorMap>();
+        var tutors = csvReader.GetRecordsAsync<Tutor>();
+        
+        return await tutors.ToListAsync();
+    }
+
+    private sealed class CsvTutorMap : ClassMap<Tutor>
+    {
+        public CsvTutorMap()
+        {
+            Map(x => x.Name).Index(0);
+            Map(x => x.Course).Index(1);
+            Map(x => x.School).Index(2);
+            Map(x => x.Ranking).Index(3);
+            Map(x => x.Exam).Index(4);
+        }
+    }
+}
