d669dc9c747eb904619faa7e9b2e8feb294aea22
zibas <milo.brontesi@gmail.com>
2022-10-21 16:55:54 +0200

    Web: added search function to reservTable

--- www/tutorbot-app/src/components/ReservationTable.jsx
+++ www/tutorbot-app/src/components/ReservationTable.jsx
@@ -9,7 +9,8 @@ class ReservationTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      ResList: props.reservations,
+      Reservations: props.reservations,
+      FilteredResList: props.reservations,
       MasterChecked: false,
       SelectedList: [],
       HeaderArrows: Array(Object.keys(props.reservations[0]).length - 1).fill(0),
@@ -19,7 +20,7 @@ class ReservationTable extends React.Component {
 
   // Select/ UnSelect Table rows
   onMasterCheck(e) {
-    let tempList = this.state.ResList;
+    let tempList = this.state.FilteredResList;
     // Check/ UnCheck All Items
     tempList.map((user) => (user.selected = e.target.checked));
 
@@ -27,13 +28,13 @@ class ReservationTable extends React.Component {
     this.setState({
       MasterChecked: e.target.checked,
       List: tempList,
-      SelectedList: this.state.ResList.filter((e) => e.selected),
+      SelectedList: this.state.FilteredResList.filter((e) => e.selected),
     });
   }
 
   // Update List Item's state and Master Checkbox State
   onItemCheck(e, item) {
-    let tempList = this.state.ResList;
+    let tempList = this.state.FilteredResList;
     console.log(item);
     tempList.map((reservation) => {
       if (reservation.id === item.id) {
@@ -43,21 +44,21 @@ class ReservationTable extends React.Component {
     });
 
     //To Control Master Checkbox State
-    const totalItems = this.state.ResList.length;
+    const totalItems = this.state.FilteredResList.length;
     const totalCheckedItems = tempList.filter((e) => e.selected).length;
 
     // Update State 
     this.setState({
       MasterChecked: totalItems === totalCheckedItems,
       List: tempList,
-      SelectedList: this.state.ResList.filter((e) => e.selected),
+      SelectedList: this.state.FilteredResList.filter((e) => e.selected),
     });
   }
 
   // Event to get selected rows(Optional)
   getSelectedRows() {
     this.setState({
-      SelectedList: this.state.ResList.filter((e) => e.selected),
+      SelectedList: this.state.FilteredResList.filter((e) => e.selected),
     });
   }
 
@@ -94,8 +95,8 @@ class ReservationTable extends React.Component {
   }
 
   sortBy(i) {
-    const tempList = this.state.ResList;
-    const keys = Object.keys(this.state.ResList[0]);
+    const tempList = this.state.FilteredResList;
+    const keys = Object.keys(this.state.FilteredResList[0]);
     switch (keys[i]) {
       case "id":
         tempList.sort((x, y) => this.comparator(x.id, y.id, this.state.HeaderArrows[i]));
@@ -125,7 +126,7 @@ class ReservationTable extends React.Component {
         break;
     }
 
-    const totalItems = this.state.ResList.length;
+    const totalItems = this.state.FilteredResList.length;
     const totalCheckedItems = tempList.filter((e) => e.selected).length;
     let selectedListTemp = this.state.SelectedList;
     let mastercheck = totalItems === totalCheckedItems;
@@ -148,18 +149,32 @@ class ReservationTable extends React.Component {
   }
 
   render() {
-    const visibleReservations = this.state.ResList.slice(0, this.state.VisibleRows);
+    const visibleRows = this.state.FilteredResList.slice(0, this.state.VisibleRows);
     return (
       <div className="cont">
         <div className="row">
 
           <div className="col-md-12">
-            <button
-              className="btn-confirm"
-              onClick={() => this.getSelectedRows()}
-            >
-              Conferma Prenotazioni Selezionate {this.state.SelectedList.length}
-            </button>
+            <div className="tableFunctions">
+              <div className="searchDiv">
+                <label htmlFor="search">
+                  Ricerca Tutor:
+                  <input id="search" type="text"  placeholder="Matr. Tutor" onChange={(e) => this.handleSearch(e, "tutorNumber")} />
+                </label>
+                <label htmlFor="search">
+                  Ricerca Studente:
+                  <input id="search" type="text" placeholder="Matr. Studente" onChange={(e) => this.handleSearch(e, "studentNumber")} />
+                </label>
+              </div>
+              <button
+                className="btn-confirm"
+                onClick={() => this.getSelectedRows()}
+              >
+                Conferma Prenotazioni Selezionate {this.state.SelectedList.length}
+              </button>
+            </div>
+
+
             <table className="table">
               <thead>
                 <tr>
@@ -191,58 +206,83 @@ class ReservationTable extends React.Component {
                 </tr>
               </thead>
               <tbody>
-        {visibleReservations.map((reservation) =>
-        (
-          <tr key={reservation.id} className={reservation.selected ? "selected" : ""}>
-            <th scope="row">
-              <input
-                type="checkbox"
-                checked={reservation.selected}
-                className="form-check-input"
-                id="rowcheck{user.id}"
-                onChange={(e) => this.onItemCheck(e, reservation)}
-              />
-            </th>
-            <td>{reservation.id}</td>
-            <td>{reservation.tutorNumber}</td>
-            <td>{reservation.tutorName}</td>
-            <td>{reservation.tutorSurname}</td>
-            <td>{reservation.examCode}</td>
-            <td>{reservation.studentNumber}</td>
-            <td>{reservation.timeStamp}</td>
-            {reservation.state ?
-              <td style={{ textAlign: 'center' }}><SquareIcon className="newStatusSquare" /></td> :
-              <td></td>}
-          </tr>
-        )
-        )}
-      </tbody>
+                {visibleRows.map((reservation) =>
+                (
+                  <tr key={reservation.id} className={reservation.selected ? "selected" : ""}>
+                    <th scope="row">
+                      <input
+                        type="checkbox"
+                        checked={reservation.selected}
+                        className="form-check-input"
+                        id="rowcheck{user.id}"
+                        onChange={(e) => this.onItemCheck(e, reservation)}
+                      />
+                    </th>
+                    <td>{reservation.id}</td>
+                    <td>{reservation.tutorNumber}</td>
+                    <td>{reservation.tutorName}</td>
+                    <td>{reservation.tutorSurname}</td>
+                    <td>{reservation.examCode}</td>
+                    <td>{reservation.studentNumber}</td>
+                    <td>{reservation.timeStamp}</td>
+                    {reservation.state ?
+                      <td style={{ textAlign: 'center' }}><SquareIcon className="newStatusSquare" /></td> :
+                      <td></td>}
+                  </tr>
+                )
+                )}
+              </tbody>
             </table>
             <ShowMoreButton onClick={() => this.handleShowMoreClick()}
               visibleRows={this.state.VisibleRows}
-              maximumRows={this.state.ResList.length}
+              maximumRows={this.state.FilteredResList.length}
             />
-            <this.renderDebug />
+            <this.renderDebug visibleList={this.state.FilteredResList} selectedList={this.state.SelectedList} />
           </div>
         </div>
       </div>
     );
   }
 
-  renderDebug() {
+  renderDebug(props) {
     if (configData.debugMode)
       return (
         <>
           <div className="row">
             <b>All Row Items:</b>
-            <code>{JSON.stringify(this.state.ResList)}</code>
+            <code>{JSON.stringify(props.visibleList)}</code>
           </div>
           <div className="row">
             <b>Selected Row Items(Click Button To Get):</b>
-            <code>{JSON.stringify(this.state.SelectedList)}</code>
+            <code>{JSON.stringify(props.selectedList)}</code>
           </div>
         </>
       )
+  }
+
+  handleSearch(event, type) {
+    let tempList;
+
+    switch (type) {
+      case "tutorNumber":
+        tempList = this.state.Reservations.filter(
+          (res) => res.tutorNumber.toString().includes(event.target.value)
+        );
+        break;
+      
+      case "studentNumber":
+        tempList = this.state.Reservations.filter(
+          (res) => res.studentNumber.toString().includes(event.target.value)
+        );
+        break;
+      default:
+        tempList = [];
+        console.error("Invalid search type");
+        break;
+    }
+    this.setState({
+      FilteredResList: tempList
+    })
   }
 }
 
@@ -280,7 +320,7 @@ function ShowMoreButton(props) {
       <div
         onClick={() => props.onClick()}
         className="btn-showmore"
-        style={{visibility: 'hidden'}}
+        style={{ visibility: 'hidden' }}
       >
         Mostra altri
       </div>
@@ -290,7 +330,7 @@ function ShowMoreButton(props) {
       <div
         onClick={() => props.onClick()}
         className="btn-showmore"
-        style={{visibility: 'visible'}}
+        style={{ visibility: 'visible' }}
       >
         Mostra altri
       </div>
@@ -298,5 +338,6 @@ function ShowMoreButton(props) {
   }
 
 }
+
 
 export default ReservationTable;
