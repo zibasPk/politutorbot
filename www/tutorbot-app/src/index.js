959c5d13301e80fe41286b441271cdb9ddb25af1
zibasPk <milo.brontesi@mail.polimi.it>
2023-01-25 18:35:47 +0100

    Revert "web: changed page routes"
    This reverts commit 900e0ba31a07abaa46366020a2c4a146e58bf64f.

--- www/tutorbot-app/src/index.js
+++ www/tutorbot-app/src/index.js
@@ -18,10 +18,10 @@ root.render(
       <div className='page'>
         <Routes>
           <Route path="/" element={<Reservations />} />
-          <Route path="#reservations" element={<Reservations />} />
-          <Route path="#active-tutorings" element={<ActiveTutorings />} />
-          <Route path="#enabled-students" element={<EnabledStudents />} />
-          <Route path="#manage-tutors" element={<TutorManagement />} />
+          <Route path="/PoliTutorBot/reservations" element={<Reservations />} />
+          <Route path="/PoliTutorBot/active-tutorings" element={<ActiveTutorings />} />
+          <Route path="/PoliTutorBot/enabled-students" element={<EnabledStudents />} />
+          <Route path="/PoliTutorBot/manage-tutors" element={<TutorManagement />} />
         </Routes>
       </div>
     </BrowserRouter>
